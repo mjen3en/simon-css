556ae77a7854df535933f29e44a150da864a70f9
Micah <batmanwillreturn@gmail.com>
2025-02-12 15:42:21 -0700

    finished transfering to react

--- src/app.jsx
+++ src/app.jsx
@@ -2,10 +2,10 @@ import React from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './app.css';
 import { BrowserRouter, NavLink, Route, Routes } from 'react-router-dom';
-import { Login } from './login/login.jsx';
-import { Play } from './play/play.jsx';
-import { Scores } from './scores/scores.jsx';
-import { About } from './about/about.jsx';
+import { Login } from './login/login';
+import { Play } from './play/play';
+import { Scores } from './scores/scores';
+import { About } from './about/about';
 
 export default function App() {
     return (
